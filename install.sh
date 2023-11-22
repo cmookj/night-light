66c9214b5420c6633f186daa57f3ea22a27b5dd3
Changmook Chun <cmookj@duck.com>
2023-11-21 23:34:10 -0800

    Fixed typo

--- install.sh
+++ install.sh
@@ -3,9 +3,9 @@
 # Install python executable
 sudo python3 -m venv /usr/local/share/venv
 source /usr/local/share/venv/bin/activate
-sudo cp ./requirements.txt /usr/local/share/venv/bin/
+# sudo cp ./requirements.txt /usr/local/share/venv/bin/
 sudo cp ./sunset_timer.py /usr/local/share/venv/bin/
-sudo cp ./LICENSE /usr/local/share/venv/bin/
+# sudo cp ./LICENSE /usr/local/share/venv/bin/
 sudo /usr/local/share/venv/bin/python3 -m pip install -r requirements.txt
 
 # Install system service
